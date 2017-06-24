183f49f32bb9ba2d79c1f44c2af920e51d5eef14
Evgeny Ugreninov <ikra.ad@mac.com>
2017-06-24 14:24:19 +0300

    transfer styles to styled components for complex area & map

---
+++ src/Complexes/Show/Area/index.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import styled from 'styled-components';
+
+
+const Area = styled.section`
+  padding-top: 4rem;
+  padding-bottom: 13.5rem;
+  background-color: #3e4247;
+`;
+
+const Wrapper = styled.div`
+padding-top: 7.25rem;
+`;
+
+const Paragraph = styled.p`
+  padding-right: 4rem;
+  margin-bottom: 3.25rem;
+  font-family: 'Philosopher';
+  font-size: 3rem;
+  font-weight: 700;
+  line-height: 1.25;
+  margin-top: 2rem;
+  color: #ffffff;
+`;
+
+const Title = styled.h2`
+  margin-top: 0;
+  margin-bottom: 3rem;
+  padding-bottom: 0;
+  font-family: 'Philosopher';
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #a9afb6;
+`;
+
+const Link = styled.a`
+text-decoration: none;
+font-family: 'Fira Sans';
+font-size: 1rem;
+color: #00779a;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+
+export default () => (
+  <Area>
+    <Grid>
+      <Row xs="between">
+        <Col xs={6}>
+          <Wrapper>
+            <Title>Якиманка</Title>
+            <Paragraph>
+              Исторический центр Москвы в двух километрах от Кремля
+            </Paragraph>
+            <Link href="undefined">Гид по Якиманке →</Link>
+          </Wrapper>
+        </Col>
+        <Col xs={6}>
+          <Image src="/images/yakimanka.png" alt="Якиманка" />
+        </Col>
+      </Row>
+    </Grid>
+  </Area>
+);
--- src/Complexes/Show/index.jsx
+++ src/Complexes/Show/index.jsx
@@ -7,6 +7,8 @@ import Summary from './Summary';
 import Description from './Description';
 import Infrastructure from './Infrastructure';
 import Offers from './Offers';
+import Area from './Area';
+import Map from './Map';
 
 const title = 'Жилой комплекс «Полянка/44»';
 const address = 'Район Якиманка, улица Большая Полянка, дом 44 · 119180';
@@ -20,47 +22,7 @@ export default () => (
     <Description />
     <Infrastructure />
     <Offers />
-    <section className="complex-area">
-      <Grid>
-        <div className="between-xs row">
-          <Col xs={6}>
-            <div className="complex-area-wrapper">
-              <h2>Якиманка</h2>
-              <p className="complex-area-paragraph">
-                Исторический центр Москвы в двух километрах от Кремля
-              </p>
-              <a className="complex-area-link" href="undefined">Гид по Якиманке →</a></div>
-          </Col>
-          <Col xs={6}>
-            <img src="/images/yakimanka.png" alt="Якиманка" />
-          </Col>
-        </div>
-      </Grid>
-    </section>
-    <section className="complex-map">
-      <Grid>
-        <Row>
-          <Col xs={6}>
-            <img className="complex-map-image" src="/images/googlemap.png" alt="" />
-          </Col>
-          <Col xs={6}>
-            <div className="complex-map-list">
-              <div className="complex-map-item">
-                <p className="complex-map-title">Красный Октябрь</p>
-                <p className="complex-map-text">24 минуты, 6 км</p>
-              </div>
-              <div className="complex-map-item">
-                <p className="complex-map-title">World className</p>
-                <p className="complex-map-text">2 минуты, 300 м</p>
-              </div>
-              <div className="complex-map-last-item">
-                <p className="complex-map-title">Третьяковская галерея</p>
-                <p className="complex-map-text">14 минуты, 4 км</p>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Grid>
-    </section>
+    <Area />
+    <Map />
   </div>
 );
