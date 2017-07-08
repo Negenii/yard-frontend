bd2d37c55dc30bcce0cb3708ee96c76f68bfe714
Evgeny Ugreninov <ikra.ad@mac.com>
2017-07-08 23:10:43 +0300

    fix: getImageUrl id presense validation

--- src/utils.js
+++ src/utils.js
@@ -11,8 +11,8 @@ export function formatAddress(location = {}) {
   return address;
 }
 
-export function getImageUrl(id = '0', size = 512) {
-  if (id !== '0') {
+export function getImageUrl(id, size = 512) {
+  if (id) {
     return `https://images.jqestate.ru/${id}-jqestate-${size}`;
   }
   return 'http://via.placeholder.com/512/?text=%D0%9D%D0%B5%D1%82+%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F';
