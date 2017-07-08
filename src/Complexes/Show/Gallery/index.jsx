584c883d249334e941cd262e1cbbef15de3f9d15
Evgeny Ugreninov <ikra.ad@mac.com>
2017-07-08 21:38:38 +0300

    fix: move AllPhotosButton to component

--- src/Complexes/Show/Gallery/index.jsx
+++ src/Complexes/Show/Gallery/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Grid } from 'react-flexbox-grid';
 import styled from 'styled-components';
-import { getImageUrl, declenNum } from '../../../utils';
+import { getImageUrl } from '../../../utils';
+import AllPhotosButton from './AllPhotosButton';
 
 const Gallery = styled.section``;
 const ImagesWrapper = styled.div`
@@ -14,27 +15,6 @@ const Image = styled.img`
   height: 400px;
 `;
 
-const ButtonWrapper = styled.div`
-  position: absolute;
-  margin-top: -2.625rem;
-`;
-
-const AllPhotosButton = styled.button`
-  padding-top: 0.5rem;
-  padding-left: 1rem;
-  padding-bottom: 0.5rem;
-  padding-right: 1rem;
-  border: none;
-  border-radius: 2px;
-  background-color: #00779a;
-  color: #fff;
-  font-family: 'Fira Sans';
-  line-height: 1;
-  font-size: 10px;
-  font-weight: 300;
-  cursor: pointer;
-`;
-
 export default props =>
   (<div>
     <Gallery>
@@ -42,12 +22,7 @@ export default props =>
         {props.images.map(image => <Image src={getImageUrl(image.id)} alt="Slider image" />)}
       </ImagesWrapper>
       <Grid>
-        <ButtonWrapper>
-          <AllPhotosButton type="button" name="button">
-            {props.images.length}{' '}
-            {declenNum(['фотография', 'фотографии', 'фотографий'])(props.images.length)}
-          </AllPhotosButton>
-        </ButtonWrapper>
+        <AllPhotosButton images={props.images} />
       </Grid>
     </Gallery>
   </div>);
